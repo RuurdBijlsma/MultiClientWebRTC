398e23e6a7d79a42b458401fb5ba10edaf5a548c
Ruurd <ruurdbijlsma@gmail.com>
2017-11-26 00:54:24 +0100

    Initial commit

---
+++ RTCClient/script.js
@@ -0,0 +1,75 @@
+//todo: mooie github maken
+//firefox fixen
+
+document.addEventListener('DOMContentLoaded', init, false);
+
+async function enableAudio() {
+    try {
+        let stream = await navigator.mediaDevices.getUserMedia({audio: true, video: false});
+        connection.reconfigureConnection({stream: stream});
+    } catch (e) {
+        alert(e.name + ', Make sure you have a microphone and that you have a connection');
+        console.log(e);
+    }
+}
+
+async function enableVideo() {
+    try {
+        let stream = await navigator.mediaDevices.getUserMedia({audio: true, video: true});
+        connection.reconfigureConnection({stream: stream});
+    } catch (e) {
+        alert(e.name + ', Make sure you have a microphone, camera and that you have a connection');
+        console.log(e);
+    }
+}
+
+function disableMedia() {
+    connection.reconfigureConnection();
+}
+
+function init() {
+    let roomPickerForm = document.querySelector('.room-picker');
+    let video = document.querySelector('.video');
+    let output = document.querySelector('.output');
+    let connectionReady = false;
+    // let connection;
+    let username;
+
+    roomPickerForm.addEventListener('submit', e => {
+        e.preventDefault();
+        let room = e.target.room.value;
+        username = e.target.username.value;
+        connection = new RTCConnection(room, "https://wake.ruurdbijlsma.com");
+        // connection = new RTCConnection(room, "https://localhost:443");
+
+        connection.on('signalServerConnect', () => console.log("Connected to signal server"));
+        connection.on('signalServerDisconnect', () => console.log("Disconnected from signal server"));
+        connection.on('peerConnect', () => {
+            console.log("Connected to peer");
+            connectionReady = true;
+        });
+        connection.on('peerDisconnect', () => console.log("Disconnected from peer"));
+        connection.on('message', data => {
+            let {sender, message} = JSON.parse(data);
+            output.innerHTML += `<p>${sender}:${message}</p>`;
+            console.log(`Received message ${sender}:${message}`);
+        });
+        connection.on('stream', stream => {
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        });
+    });
+
+    let messageForm = document.querySelector('.message-sender');
+    messageForm.addEventListener('submit', e => {
+        e.preventDefault();
+        let message = e.target.message.value;
+        if (connectionReady) {
+            output.innerHTML += `<p>me:${message}</p>`;
+            connection.broadcast('message', JSON.stringify({sender: username, message: message}));
+            e.target.message.value = '';
+        } else {
+            alert("Connection is not yet ready")
+        }
+    });
+}
---
+++ RTCServer/index.js
@@ -0,0 +1,55 @@
+var https = require('https'),
+    fs = require('fs'),
+    maxClients = 4,
+    options = {
+        key: fs.readFileSync('../Certificate/key.pem'),
+        cert: fs.readFileSync('../Certificate/cert.pem'),
+    },
+    server = https.createServer(options),
+    port = 443,
+    io = require('socket.io')(server);
+
+server.listen(port);
+console.log('Now listening on ' + port);
+
+
+io.on('connection', socket => {
+    var ip = socket.request.connection.remoteAddress.split(':').splice(-1)[0];
+    console.log("Client connected with ip:", ip);
+
+    socket.emit('socketId', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log("Client disconnected");
+    });
+
+    socket.on('signal', (signal, peerId, success) => {
+        console.log(`Received signal event, sending to peer with id: ${peerId}`);
+        io.sockets.connected[peerId].emit('signal', signal, socket.id);
+        success(true, `sent signal to ${peerId}`);
+    });
+
+    socket.on('createOrJoin', (room, success) => {
+        console.log("Received request to create or join room", room);
+        var numClients = getRoomClientCount(room);
+
+        if (numClients === 0) {
+            socket.join(room);
+            success(true, 'created');
+            // console.log(socket.id + " created room " + room);
+        } else if (numClients < maxClients) {
+            socket.join(room);
+            socket.broadcast.to(room).emit('join', socket.id);
+            success(true, 'joined');
+        } else {
+            success(false, 'full');
+        }
+
+        console.log(`Total clients in room ${room} now ${getRoomClientCount(room)}`);
+    });
+});
+
+function getRoomClientCount(room) {
+    var clientsInRoom = io.sockets.adapter.rooms[room];
+    return clientsInRoom === undefined ? 0 : clientsInRoom.length;
+}
