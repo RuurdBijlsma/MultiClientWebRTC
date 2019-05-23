577e475531db1cffbf092b0d8543147f2c49b87e
Ruurd Bijlsma <ruurd@bijlsma.dev>
2019-05-23 22:41:50 +0200

    Revert

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,59 @@
-var app = require('express')();
-var https = require('https');
-var fs = require('fs');
-var server = https.createServer({
-    key: fs.readFileSync('/etc/letsencrypt/live/rtc.ruurd.dev/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/rtc.ruurd.dev/fullchain.pem'),
-    requestCert: false,
-    rejectUnauthorized: false
-}, app);
-server.listen(5500);
+const https = require('https'),
+    fs = require('fs'),
+    maxClients = 4,
+    options = {
+        // Real cert
+        key: fs.readFileSync('/etc/letsencrypt/live/rtc.ruurd.dev/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/rtc.ruurd.dev/fullchain.pem'),
+        // Development cert
+        // key: fs.readFileSync('../Certificate/key.pem'),
+        // cert: fs.readFileSync('../Certificate/cert.pem'),
+    },
+    server = https.createServer(options),
+    port = 5858,
+    io = require('socket.io')(server);
 
-var io = require('socket.io').listen(server, {'transports': ['websocket', 'polling']});
+server.listen(port);
+console.log('Now listening on ' + port);
 
-io.sockets.on('connection', function (socket) {
-    console.log(socket);
+
+io.on('connection', socket => {
+    const ip = socket.request.connection.remoteAddress.split(':').splice(-1)[0];
+    console.log("Client connected with ip:", ip);
+
+    socket.emit('socketId', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log("Client disconnected");
+    });
+
+    socket.on('signal', (signal, peerId, success) => {
+        console.log(`Received signal event, sending to peer with id: ${peerId}`);
+        io.sockets.connected[peerId].emit('signal', signal, socket.id);
+        success(true, `sent signal to ${peerId}`);
+    });
+
+    socket.on('createOrJoin', (room, success) => {
+        console.log("Received request to create or join room", room);
+        const numClients = getRoomClientCount(room);
+
+        if (numClients === 0) {
+            socket.join(room);
+            success(true, 'created');
+            // console.log(socket.id + " created room " + room);
+        } else if (numClients < maxClients) {
+            socket.join(room);
+            socket.broadcast.to(room).emit('join', socket.id);
+            success(true, 'joined');
+        } else {
+            success(false, 'full');
+        }
+
+        console.log(`Total clients in room ${room} now ${getRoomClientCount(room)}`);
+    });
 });
 
-app.get("/", function (request, response) {
-    response.send("DOEI");
-})
+function getRoomClientCount(room) {
+    const clientsInRoom = io.sockets.adapter.rooms[room];
+    return clientsInRoom === undefined ? 0 : clientsInRoom.length;
+}
